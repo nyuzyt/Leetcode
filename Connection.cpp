91e90cdc626df5d421cf4f7292dc46c3bd8511ab
nyuzyt <jstzzyt01@gmail.com>
2019-04-13 15:18:58 -0400

    add connection class

---
+++ Connection.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Zhou Yitao on 2019-04-13.
+//
+
+#include "Connection.h"
+
+Connection::Connection(string node1, string node2, int cost) {
+    this->node1 = node1;
+    this->node2 = node2;
+    this->cost = cost;
+}
---
+++ Connection.h
@@ -0,0 +1,19 @@
+//
+// Created by Zhou Yitao on 2019-04-13.
+//
+
+#ifndef LEETCODE_CONNECTION_H
+#define LEETCODE_CONNECTION_H
+
+#include <string>
+using namespace std;
+
+class Connection {
+    string node1;
+    string node2;
+    int cost;
+    Connection(string node1, string node2, int cost);
+};
+
+
+#endif //LEETCODE_CONNECTION_H
